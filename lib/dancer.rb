5ddf9750f3e4da012725c6c7714845bddfb18861
Tammy Lam <lamtammy1515@gmail.com>
2020-02-26 05:19:10 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,9 +2,9 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-
+ includes Dance
    extend MetaDancing
-   includes Dance
+  
  
   attr_accessor :name
  
